3e07c7b4977c900201e85d192dcaa8da2b73e894
Michiel <michielalexanderb@gmail.com>
2022-10-05 16:26:24 +0100

    Nix support (#2)

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A CLI-tool to diff Keepass (.kdbx) files.";
+
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, cargo2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.64.0";
+          packageFun = import ./Cargo.nix;
+        };
+      in
+      {
+        packages = {
+          keediff = (rustPkgs.workspace.keepass-diff { }).bin;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells = {
+          default = (rustPkgs.workspaceShell { });
+        };
+      }
+    );
+}
